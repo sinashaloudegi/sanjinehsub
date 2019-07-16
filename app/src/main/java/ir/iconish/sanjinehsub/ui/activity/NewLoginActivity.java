dc8f5f9cacb2dbf8dcaffc602a3b1e181ac9737d
s.shaloudegi <sinashaloudegi@gmail.com>
2019-07-16 17:51:53 +0430

    adding new login page

---
+++ app/src/main/java/ir/iconish/sanjinehsub/ui/activity/NewLoginActivity.java
@@ -0,0 +1,16 @@
+package ir.iconish.sanjinehsub.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ir.iconish.sanjinehsub.R;
+
+public class NewLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_login);
+    }
+}
